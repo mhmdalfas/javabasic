079bbd62d2ce6ccb5ab9bc5d4ec39bcfaab6018a
mhmdalfas <muhammedalfas13@gmail.com>
2021-06-16 19:43:03 +0530

    showsum

---
+++ src/javabasic/ShowSum.java
@@ -0,0 +1,15 @@
+package javabasic;
+
+public class ShowSum {
+	
+	 public static void main(String[] args) {
+		 showSum(4,5);
+		
+		 
+	 }
+	 static void showSum(int a, int b)
+	 {
+		 System.out.println(a+b);
+	 }
+	 
+}
