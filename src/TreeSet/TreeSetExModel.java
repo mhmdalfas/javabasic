82ebdd7d798672556c7e539026bae20ebfbe2928
mhmdalfas <muhammedalfas13@gmail.com>
2021-06-21 23:18:25 +0530

    tree

---
+++ src/TreeSet/TreeSetExModel.java
@@ -0,0 +1,52 @@
+package TreeSet;
+
+public class TreeSetExModel implements Comparable<TreeSetExModel>{
+	
+	String name;
+	int empId;
+	int Phone;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public int getPhone() {
+		return Phone;
+	}
+	public void setPhone(int phone) {
+		Phone = phone;
+	}
+	public TreeSetExModel() {
+		super();
+		
+	}
+	public TreeSetExModel(String name, int empId, int phone) {
+		super();
+		this.name = name;
+		this.empId = empId;
+		Phone = phone;
+	}
+	@Override
+	public String toString() {
+		return "TreeSetExModel [name=" + name + ", empId=" + empId + ", Phone=" + Phone + "]";
+	}
+	@Override
+	public int compareTo(TreeSetExModel o) {
+		
+		return 0;
+	}
+	
+	
+	
+	
+
+	
+	
+}
